9f977d25508a6786468a82d2ddbf5965046611f8
jonbrand <jonbranaraujo18@gmail.com>
2021-09-30 09:32:47 -0300

    feat: add input and button

--- ignite/trilha-react-native/chapter-1/src/pages/Home.js
+++ ignite/trilha-react-native/chapter-1/src/pages/Home.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  TextInput, 
+  Platform,
+  TouchableOpacity 
+} from 'react-native';
 
 export function Home() {
   
   return (
     <>
-      <View style={styles.container}>
-        <Text>Jonatas</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Welcome, Jonatas</Text>
+
+        <TextInput 
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#999"
+        />
+
+        <TouchableOpacity 
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, { marginTop: 20 }]}>
+          My Skills
+        </Text>
+      </SafeAreaView>
     </>
   )
 }
@@ -15,7 +39,34 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 17,
+    fontWeight: 'bold',
   }
 })
